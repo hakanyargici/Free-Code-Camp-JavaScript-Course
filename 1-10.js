e31569f2fd583d86da7fe9fd043cbbd9a3180604
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-03-21 15:25:13 +0300

    app.js dosyası 1-10.js ile değişti ve içeriği 1'den 10'a kadar olan soruların başlıkları ve çözümleri ile güncellendi.

---
+++ 1-10.js
@@ -0,0 +1,80 @@
+// Quest 1 - Comment Your JavaScript Code
+
+// Comment
+
+/* Comment */
+
+// Quest 2 - Declare JavaScript Variables
+
+// var myName;
+
+// Quest 3 - Storing Values with the Assignment Operator
+
+// // Setup
+// var a;
+
+// // Only change code below this line
+
+// a = 7
+
+// Quest 4 - Assigning the Value of One Variable to Another
+
+// // Setup
+// var a;
+// a = 7;
+// var b;
+
+// // Only change code below this line
+
+// b = a;
+
+// Quest 5 - Initializing Variables with the Assignment Operator
+
+// var myVar = 0;
+
+// var a = 9;
+
+// Quest 6 - Declare String Variables
+
+// var myFirstName = "M";
+// var myLastName = "P";
+
+// Quest 7 - Understanding Uninitialized Variables
+
+// // Only change code below this line
+// var a;
+// var b;
+// var c;
+// // Only change code above this line
+
+// a = 5;
+// b = 10;
+// c = "I am a"
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// Quest 8 - Understanding Case Sensitivity in Variables
+
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// Quest 9 - Explore Differences Between the var and let Keywords
+
+// let catName = "Oliver";
+// let catSound = "Meow!";
+
+// Quest 10 - Declare a Read-Only Variable with the const Keyword
+
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(FCC, fact); // Change this line
