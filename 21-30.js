7bb37ea5caa30a492d7080c313340963b04e873e
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-03-21 16:53:59 +0300

    21-30 arası tüm soruların cevapları dosya içerisine dahil edilmiştir.

---
+++ 21-30.js
@@ -0,0 +1,73 @@
+// Quest 21 - Compound Assignment With Augmented Addition
+
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// // Only change code below this line
+
+// a += 12;
+// b += 9;
+// c += 7;
+
+// Quest 22 - Compound Assignment With Augmented Subtraction
+
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// // Only change code below this line
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+// Quest 23 - Compound Assignment With Augmented Multiplication
+
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+
+// // Only change code below this line
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+// Quest 24 - Compound Assignment With Augmented Division
+
+// let a = 48;
+// let b = 108;
+// let c = 33;
+
+// // Only change code below this line
+// a /= 12;
+// b /= 4;
+// c /= 11;
+
+// Quest 25 - Escaping Literal Quotes in Strings
+
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+// Quest 26 - Quoting Strings with Single Quotes
+
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Quest 27 - Escape Sequences in Strings
+
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// Quest 28 - Concatenating Strings with Plus Operator
+
+// const myStr = "This is the start. " + "This is the end."; // Change this line
+
+// Quest 29 - Concatenating Strings with the Plus Equals Operator
+
+// let myStr = "This is the first sentence. ";
+
+// myStr += "This is the second sentence.";
+
+// Quest 30 - Constructing Strings with Variables
+
+// const myName = "My name is ";
+// const myStr = " and I am well!";
+
+// const ourStr = "Hi," + myName + "Hakan" + myStr;
