f48e983b4d0eee7b697c7175b7eee607713106f1
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-03-21 16:13:39 +0300

    11-20.js içeriği komple değişmiş olup, doğru yanıtları ile eklenmiştir.

---
+++ 11-20.js
@@ -0,0 +1,49 @@
+// Quest 11 - Add Two Numbers with JavaScript
+
+// const sum = 10 + 10;
+
+// Quest 12 - Subtract One Number from Another with JavaScript
+
+// const difference = 45 - 33;
+
+// Quest 13 - Multiply Two Numbers with JavaScript
+
+// const product = 8 * 10;
+
+// Quest 14 - Divide One Number by Another with JavaScript
+
+// const quotient = 66 / 33;
+
+// Quest 15 - Increment a Number with JavaScript
+
+// let myVar = 87;
+
+// // Only change code below this line
+// myVar++;
+
+// Quest 16 - Decrement a Number with JavaScript
+
+// let myVar = 11;
+
+// // Only change code below this line
+// myVar--;
+
+// Quest 17 - Create Decimal Numbers with JavaScript
+
+// const ourDecimal = 5.7;
+
+// // Only change code below this line
+
+// const myDecimal = 5.3;
+
+// Quest 18 - Multiply Two Decimals with JavaScript
+
+// const product = 5.0 * 1.0;
+
+// Quest 19 - Divide One Decimal by Another with JavaScript
+
+// const quotient = 4.4 / 2.0; // Change this line
+
+// Quest 20 - Finding a Remainder in JavaScript
+
+// const remainder = 5 % 3;
