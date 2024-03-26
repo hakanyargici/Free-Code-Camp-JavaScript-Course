30be7520fc0c2a42efe430262b83b8acbcb79f61
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-03-26 15:41:46 +0300

    31-40.js dosyası eksiksiz ve doğru yanıtları içeriğinde barındırarak güncelledi.

---
+++ 31-40.js
@@ -0,0 +1,79 @@
+// Quest 31 - Appending Variables to Strings
+
+// Change code below this line
+const someAdjective = "perfect!";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+// Quest 32 - Find the Length of a String
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Quest 33 - Use Bracket Notation to Find the First Character in a String
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Quest 34 - Understand String Immutability 
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World";
+// Only change code above this line
+
+// Quest 35 - Use Bracket Notation to Find the Nth Character in a String
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Quest 36 - Use Bracket Notation to Find the Last Character in a String
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Quest 37 - Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+// Quest 38 - Word Blanks
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun+ " " + myAdjective+ " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+// Quest 39 - Store Multiple Values in one Variable using JavaScript Arrays
+
+// Only change code below this line
+const myArray = ["Hakan", 1];
+
+// Quest 40 - Nest one Array within Another Array
+
+// Only change code below this line
+const myArray = [["Hakan"], ["Yargıcı"]];
