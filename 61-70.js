9854ffff1b4e92e71267fc204bffa0b551a0fb34
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-03-28 16:12:39 +0300

    61-70 arasında bulunan tüm soruların cevapları proje içine dahil edildi.

---
+++ 61-70.js
@@ -0,0 +1,141 @@
+// Quest 61 - Practice comparing different values
+
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
+// Quest 62 - Comparison with the Inequality Operator
+
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
+
+// Quest 63 - Comparison with the Strict Inequality Operator
+
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
+
+// Quest 64 - Comparison with the Greater Than Operator
+
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
+
+// Quest 65 - Comparison with the Greater Than Or Equal To Operator
+
+function testGreaterOrEqual(val) {
+    if (val >= 20) {  // Change this line
+      return "20 or Over";
+    }
+  
+    if (val >= 10) {  // Change this line
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
+
+// Quest 66 - Comparison with the Less Than Operator
+
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+
+// Quest 67 - Comparison with the Less Than Or Equal To Operator
+
+function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val <= 24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  testLessOrEqual(10);
+
+// Quest 68 - Comparisons with the Logical And Operator
+
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val > 24 && val < 51) {
+      return "Yes";
+    } else {
+      // Only change code above this line
+      return "No";
+    }
+  }
+  testLogicalAnd(10);
+
+// Quest 59 - Comparisons with the Logical Or Operator
+
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    } else {
+      result = "5 or Smaller";
+    }
+  
+      return result;
+  }
+  
+  testElse(4);
+
+// Quest 60 - Introducing Else If Statements
+
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if (val < 5) {
+      return "Smaller than 5";
+    } else{
+      return "Between 5 and 10";
+    }
+  }
+  
+  testElseIf(7);
