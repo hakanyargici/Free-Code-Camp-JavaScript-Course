39742311de6305348ddef9dedd40cb2b21b3ae04
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-04-08 22:51:18 +0300

    6. Soruya kadar eklendi.

--- 101.113.js
+++ 101.113.js
@@ -38,3 +38,44 @@ function sum(arr, n) {
       }
     // Only change code above this line
 }
+
+// Quest 104 - Profile Lookup
+
+function lookUpProfile(name, prop) {
+    // Only change code below this line
+    for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
+    // Only change code above this line
+}
+
+// Quest 105 - Generate Random Fractions with JavaScript
+
+function randomFraction() {
+    // Only change code below this line
+    let result = 0;
+    while (result === 0) {
+      result = Math.random();
+    }
+    return result;
+    // Only change code above this line
+}
+
+// Quest 106 - Generate Random Whole Numbers with JavaScript
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+  // Only change code below this line.
+  return Math.floor(Math.random() * 10);
+}
+
+// Quest 107 - Generate Random Whole Numbers within a Range
+
