4d694756db39881a46df41052b26f62b82554261
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-04-22 15:02:18 +0300

    112. soru yükledni.

--- 101.113.js
+++ 101.113.js
@@ -119,4 +119,14 @@ function checkSign(num) {
     : "positive"
 }
 
+// Quest 112 - Use Recursion to Create a Countdown
 
+function countdown(n){
+  if (n < 1) {
+    return [];
+    } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
